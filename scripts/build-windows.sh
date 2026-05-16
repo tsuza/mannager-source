10e454af15ca3189de00d198c36535b85f6cbbd1
tsuza <tsuza@proton.me>
2026-05-16 20:30:42 +0200

    fix

--- scripts/build-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-EXE_NAME="mannager.exe"
-TARGET="x86_64-pc-windows-msvc"
-VERSION=$(cargo pkgid | cut -d '@' -f 2)
-
-# build binary
-rustup target add $TARGET
-cargo build --release --target=$TARGET
-cp -fp target/$TARGET/release/$EXE_NAME target/release
